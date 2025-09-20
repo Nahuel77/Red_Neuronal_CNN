16efbb4a56e80413b344d495285d0e9be3748e3a
Nahuel Astor <nahuel89astor@gmail.com>
2025-09-20 15:19:58 -0300

    capas

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-"# Red_Neuronal_CNN" 
+Red Neuronal Convolucional (CNN) que detecta números escritos a mano.
+
+Anteriormente habia creado una red MLP que realizaba la misma tarea.
+
+Ahora hare el paso de MLP a CNN con el proposito de mejorar la presición
+
+Arquitectura típica de una CNN
+
+Entrada (imagen) 
+       ↓
+Convolución + ReLU
+       ↓
+Pooling
+       ↓
+Convolución + ReLU
+       ↓
+Pooling
+       ↓
+Flatten
+       ↓
+Fully Connected
+       ↓
+Softmax (clasificación)
+
+<h3>Si bien en MLP iniciabamos pesos al azar y los ajustabamos con retropropagación. Aqui lo que se iniciará y ajustaran seran los Kernels de las convoluciones.</h3>
---
+++ main.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+train = pd.read_csv('data_set/train.csv')
+
+y_train = train['label'].values
+x_train = train.drop(columns=['label']).values / 255.0
+
+x_train = x_train.reshape(-1, 28, 28, 1)
+
+x_train, x_val, y_train, y_val = train_test_split(
+    x_train, y_train, test_size=0.2, random_state=42
+)
+
+y_train = to_categorical(y_train, 10)
+y_val = to_categorical(y_val, 10)
+
+# Definir la arquitectura de la CNN
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D((2, 2)),
+    Conv2D(63, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(10, activation='softmax')
+])
+
+# Compilar el modelo
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Resumen
+model.summary()
+
+# Entrenamiento
+history = model.fit(
+    x_train, y_train,
+    validation_data=(x_val, y_val),
+    epochs=10,
+    batch_size=32
+)
